621bc2737c82ca8d5505f7b9d9f8190b826357e2
Harsha <harshaborkhade24@gmail.com>
2024-02-07 12:22:18 +0530

    second commit

---
+++ StateViewApplication/Controllers/StateController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StateViewApplication.Data;
+using StateViewApplication.Models;
+using Newtonsoft.Json;
+
+namespace StateViewApplication.Controllers
+{
+    public class StateController : Controller
+    {
+        private readonly DatabaseHelper _databaseHelper;
+
+        public StateController(string connectionString)
+        {
+            _databaseHelper = new DatabaseHelper(connectionString);
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<TreeViewNode> nodes = _databaseHelper.GetNodes();
+
+            //Serialize to JSON string.
+            ViewBag.Json = JsonConvert.SerializeObject(nodes);
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Index(string selectedItems)
+        {
+            if (string.IsNullOrEmpty(selectedItems))
+            {
+                // Handle the error condition, e.g. redirect back to the index page
+                return RedirectToAction("Index");
+            }
+
+            List<TreeViewNode> items = JsonConvert.DeserializeObject<List<TreeViewNode>>(selectedItems);
+
+            foreach (TreeViewNode item in items)
+            {
+                if (item.id.Contains('-'))
+                {
+                    _databaseHelper.InsertNode(item);
+                }
+                else
+                {
+                    _databaseHelper.UpdateNode(item);
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public JsonResult GetNodes()
+        {
+            List<TreeViewNode> nodes = _databaseHelper.GetNodes();
+            return Json(nodes);
+        }
+    }
+}
---
+++ StateViewApplication/Data/DatabaseHelper.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Data.SqlClient;
+using StateViewApplication.Models;
+using System.Data;
+
+namespace StateViewApplication.Data
+{
+    public class DatabaseHelper
+    {
+        private string _connectionString;
+
+        public DatabaseHelper(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<TreeViewNode> GetNodes()
+        {
+            List<TreeViewNode> nodes = new List<TreeViewNode>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("dbo.GetNodes", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            nodes.Add(new TreeViewNode
+                            {
+                                id = reader["Id"].ToString(),
+                                parent = reader["ParentId"].ToString(),
+                                text = reader["Text"].ToString()
+                            });
+                        }
+                    }
+                }
+            }
+
+            return nodes;
+        }
+
+        public void InsertNode(TreeViewNode node)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("dbo.InsertNode", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Id", node.id);
+                    command.Parameters.AddWithValue("@ParentId", node.parent);
+                    command.Parameters.AddWithValue("@Text", node.text);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void UpdateNode(TreeViewNode node)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("dbo.UpdateNode", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Id", node.id);
+                    command.Parameters.AddWithValue("@ParentId", node.parent);
+                    command.Parameters.AddWithValue("@Text", node.text);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void DeleteNode(string id)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("dbo.DeleteNode", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
